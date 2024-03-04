5c8fdc25370888f7963bd7d3867cfe05bcab84a9
Yifan Yang <yifany5@nycstudents.net>
2024-03-03 20:03:35 -0500

    Added shuffler

--- src/CardTester.java
+++ src/CardTester.java
@@ -8,6 +8,10 @@ public class CardTester {
      *	@param args is not used.
      */
     public static void main(String[] args) {
-        /* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+        Card card1 = new Card("Jack", "Spade", 11);
+        Card card2 = new Card("3", "Diamonds", 3);
+        Card card3 = new Card("Jack", "Spade", 11);
+        System.out.println(card1.matches(card3));
+        System.out.println(card1);
     }
 }
